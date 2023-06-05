2e481a8cda89baa740bc06aa8de52b27de124f9b
coolcatmeow <trey.dew@outlook.com>
2023-06-05 18:29:16 -0500

    update

---
+++ src/currencyInput.js
@@ -0,0 +1,11 @@
+function CurrencyInput(props){
+    return(
+        <div className="group">
+            <input type="text" value={1}/>
+            <select></select>
+
+        </div>
+    );
+}
+
+export default CurrencyInput;
