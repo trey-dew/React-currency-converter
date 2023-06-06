88b0548a2fdcbb9f432a7c7750b66a7a2805b21f
coolcatmeow <trey.dew@outlook.com>
2023-06-06 14:43:06 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
 const [amount1, setAmount1] = useState(1);
 const [amount2, setAmount2] = useState(1);
 const [currency1, setCurrency1] = useState('USD')
-const [currency2, setCurrency2] = useState('USD')
+const [currency2, setCurrency2] = useState('EUR')
 const [rates, setRates] = useState([]);
 
 const url = 'http://data.fixer.io/api/latest?access_key=';
@@ -20,18 +20,49 @@ useEffect(() =>{
   })
 }, []);
 
+useEffect(() => {
+  if(!!rates){
+    handleAmount1Change(1);
+  }
+}, [rates]);
+
+function format(number){
+  return number.toFixed(4);
+}
+
+function handleAmount1Change(amount1){
+  setAmount2(format(amount1 * rates[currency2]/ rates[currency1]));
+  setAmount1(amount1);
+}
+
+function handleCurrency1Change(currency1){
+  setAmount2(format(amount1 * rates[currency2]/ rates[currency1]));
+  setCurrency1(currency1);
+}
+
+function handleAmount2Change(amount2){
+  setAmount1(format(amount2 * rates[currency1]/ rates[currency2]));
+  setAmount2(amount2);
+}
+
+function handleCurrency2Change(currency2){
+  setAmount1(format(amount2 * rates[currency1]/ rates[currency2]));
+  setCurrency2(currency2);
+}
+
   return (
-    <div > 
+    <div >
+      <h1>Currency Converter</h1> 
       <CurrencyInput 
-        onAmountChange={setAmount1}
-        onCurrencyChange={setCurrency1}
+        onAmountChange={handleAmount1Change}
+        onCurrencyChange={handleCurrency1Change}
         currencies={Object.keys(rates)} 
         amount={amount1} 
         currency={currency1}/>
 
       <CurrencyInput
-        onAmountChange={setAmount2}
-        onCurrencyChange={setCurrency2} 
+        onAmountChange={handleAmount2Change}
+        onCurrencyChange={handleCurrency2Change} 
         currencies={Object.keys(rates)} 
         amount={amount2} 
         currency={currency2}/>
