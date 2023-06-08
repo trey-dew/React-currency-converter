3183b2da5d782c85cc77872c17698bd8c36087c0
coolcatmeow <trey.dew@outlook.com>
2023-06-07 19:24:10 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import CurrencyInput from './currencyInput';
 import axios from 'axios';
 
+var historyArray = [];
+
 function App() {
 
 const [amount1, setAmount1] = useState(1);
@@ -12,6 +14,8 @@ const [currency2, setCurrency2] = useState('EUR')
 const [rates, setRates] = useState([]);
 
 const url = 'http://data.fixer.io/api/latest?access_key=';
+
+
 
 useEffect(() =>{
   axios.get(url)
@@ -33,26 +37,38 @@ function format(number){
 function handleAmount1Change(amount1){
   setAmount2(format(amount1 * rates[currency2]/ rates[currency1]));
   setAmount1(amount1);
+  historyCalculator()
 }
 
 function handleCurrency1Change(currency1){
   setAmount2(format(amount1 * rates[currency2]/ rates[currency1]));
   setCurrency1(currency1);
+  historyCalculator()
 }
 
 function handleAmount2Change(amount2){
   setAmount1(format(amount2 * rates[currency1]/ rates[currency2]));
   setAmount2(amount2);
+  historyCalculator()
 }
 
 function handleCurrency2Change(currency2){
   setAmount1(format(amount2 * rates[currency1]/ rates[currency2]));
   setCurrency2(currency2);
+  historyCalculator()
+}
+
+function historyCalculator()
+{
+  var historyElement = amount1 + " " + currency1 + " " + " = " + amount2 + " " + currency2;
+  historyArray.unshift(historyElement);  
+
 }
 
   return (
     <div >
-      <h1>Currency Converter</h1> 
+      
+      <h1>Currency Converter</h1>       
       <CurrencyInput 
         onAmountChange={handleAmount1Change}
         onCurrencyChange={handleCurrency1Change}
@@ -66,6 +82,13 @@ function handleCurrency2Change(currency2){
         currencies={Object.keys(rates)} 
         amount={amount2} 
         currency={currency2}/>
+
+      <h2>History</h2>      
+      <ul>
+        {historyArray.map((item,index) =>
+          <li key={index}>{item}</li>
+        )}
+      </ul>      
     </div>
   );
 }
